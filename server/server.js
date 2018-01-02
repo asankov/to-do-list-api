e7f9b48b7949b1c025247dd5c4ba72af8366004b
Anton Sankov <a.sankov96@gmail.com>
2018-01-02 22:05:03 +0200

    Added test for GET /todos

--- server/server.js
+++ server/server.js
@@ -24,10 +24,13 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
 
     Todo.find().then(todos => {
+        if (todos.length === 0) {
+            res.status(204);
+        }
         res.send({todos});
     }, error => {
         res.status(400).send(error);
-    })
+    });
 });
 
 app.listen(3000, () => {
