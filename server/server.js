a1763041abde17432449ae0810206e45881adc57
Anton Sankov <a.sankov96@gmail.com>
2018-01-02 21:42:56 +0200

    Added GET /todos endpoint

--- server/server.js
+++ server/server.js
@@ -21,6 +21,19 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+
+    Todo.find().then(todos => {
+        res.send({todos});
+    }, error => {
+        res.status(400).send(error);
+    })
+});
+
 app.listen(3000, () => {
     console.log('Magic happens on 3000');
 });
+
+module.exports = {
+    app
+};
