ca6adce337d86df6d46cb9c9b014671a7ba35129
Anton Sankov <a.sankov96@gmail.com>
2018-01-15 03:38:03 +0200

    Updated user model - added email validation and password field

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-var User = mongoose.model('User', {
+const User = mongoose.model('User', {
     name: {
         type: String,
         required: true,
@@ -8,8 +9,28 @@ var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email.'
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 });
 
 module.exports = { User };
