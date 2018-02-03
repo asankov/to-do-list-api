41e08cc00243af01620700582d18d4667ac8e6f0
Anton Sankov <a.sankov96@gmail.com>
2018-02-03 21:22:23 +0200

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -6,8 +6,7 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: true,
+        type: String
     },
     email: {
         type: String,
@@ -73,6 +72,25 @@ userSchema.statics.findByToken = function (token) {
       'tokens.token': token,
       'tokens.access': 'auth'
   });
+};
+
+userSchema.statics.findByCredentials = function(email, password) {
+    const User = this;
+
+    return User.findOne({email}).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                }
+                reject();
+            })
+        })
+    })
 };
 
 userSchema.pre('save', function(next) {
--- server/server.js
+++ server/server.js
@@ -114,6 +114,19 @@ app.post('/users', (req, res) => {
     })
 });
 
+app.post('/users/login', (req, res) => {
+   const body = _.pick(req.body, ['email', 'password']);
+
+   User.findByCredentials(body.email, body.password).then(user => {
+       return user.generateAuthToken().then(token => {
+           res.header('x-auth', token).send(user);
+       })
+   }).catch(err => {
+        res.status(400).send();
+   });
+
+});
+
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
 
