616b58c6cf13672858961e287e2f11613c4e7cf3
Anton Sankov <asankov96@gmail.com>
2018-11-25 00:07:40 +0200

    Add DELETE /users/me/token endpoint

--- server/models/user.js
+++ server/models/user.js
@@ -57,6 +57,12 @@ userSchema.methods.generateAuthToken = function() {
     });
 };
 
+userSchema.methods.removeToken = function(token) {
+    var user = this;
+
+    return user.update({ $pull: { tokens: { token }}})
+}
+
 userSchema.statics.findByToken = function (token) {
   const User = this;
   var decoded;
--- server/server.js
+++ server/server.js
@@ -149,6 +149,13 @@ app.get('/usersme', authenticate, (req, res) => {
    res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(
+        () => res.status(200).send(),
+        () => res.status(400).send()
+    )
+})
+
 app.listen(port, () => {
     console.log(`Magic happens on ${port}`);
 });
