9945fe767961bf8b2b8f4e882c36ebb54b021b28
asankov <a.sankov96@gmail.com>
2018-12-28 00:31:18 +0200

    Add Dockerfile

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+const option = {
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  reconnectTries: 30000
+};
+
+mongoose.connect(process.env.MONGODB_URI, option, e => console.log(e));
 
 module.exports = {
   mongoose
